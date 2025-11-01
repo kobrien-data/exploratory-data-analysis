27d17c7b1ade6f4055826df95f19afeea75fe82c
Kai O Brien <karenelizabeth28597@gmail.com>
2025-11-01 17:43:09 +0100

    updated README, checked for and fixed missing values in the data

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# exploratory-data-analysis-staticdata
-repo for doing an exploratory data analysis project using static data
+# Exploratory Data Analysis of Horror Movies
+This project is to do an exploratory data analysis project on horror movies. The dataset comes from:
+> https://www.kaggle.com/datasets/PromptCloudHQ/imdb-horror-movie-dataset
+
+## Tools Used
+To do this data analysis, I am using python, pandas, and matplotlib.
+
+## First Steps
+The first step I took was to check for missing values in the data and fill them. For this dataset, I chose to fill the missing values with 'Not Rated', 'Unknown', and 0 for review_rating which is type float. 
+
+
+## Analysis
--- main.py
+++ main.py
@@ -5,3 +5,50 @@ import seaborn as sns
 
 df = pd.read_csv("imdb_horror_movies.csv")
 print(df.head())
+print(df.shape)
+print(df.info())
+
+#rename columns for easier access
+df.rename(columns={'Title':'title', 
+                   'Genres':'genre', 
+                   'Release Date': 'release_date', 
+                   'Release Country' : 'release_country',
+                   'Movie Run Time':'runtime', 
+                   'Movie Rating':'movie_rating', 
+                   'Review Rating':'review_rating', 
+                   'Plot':'plot', 
+                   'Cast':'cast', 
+                   'Language': 'language', 
+                   'Filming Locations' : 'filming_locations', 
+                   'Budget' : 'budget'}, inplace=True)
+
+# Check for missing values
+print(df.isnull().sum())
+
+# Fill missing values in 'genre' with 'Unknown'
+df['genre'].fillna('Unknown', inplace=True)
+
+# Fill missing values in 'movie_rating' with 'Not Rated'
+df['movie_rating'].fillna('Not Rated', inplace=True)
+
+# Fill missing values in 'review_rating' with 'Not Rated'
+df['review_rating'].fillna(0.00, inplace=True)
+
+# Fill missing values in 'runtime' with the 'Unknown'
+df['runtime'].fillna('Unknown', inplace=True)
+
+# Fill missing values in 'plot' with the 'Unknown'
+df['plot'].fillna('Unknown', inplace=True)
+
+# Fill missing values in 'cast' with the 'Unknown'
+df['cast'].fillna('Unknown', inplace=True)
+
+# Fill missing values in 'language' with the 'Unknown'
+df['language'].fillna('Unknown', inplace=True)
+
+# Fill missing values in 'filming_locations' with 'Unknown'
+df['filming_locations'].fillna('Unknown', inplace=True)
+
+# Fill missing values in 'budget' with null
+df['budget'].fillna('Unknown', inplace=True)
+
